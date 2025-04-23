8ef0579b8f138c23fc312beb4639f49e673dc400
OmarChouchane <omar.ch52831@gmail.com>
2025-04-23 20:08:08 +0100

    setting navbar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Header = () => {
   return (
-    <div>
-        Header
-    </div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image 
+            src="/assets/images/logo.svg" alt="EventlyLogo" width={128} height={39}
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          
+        </div>
+      </div>
+    </header>
   )
 }
 
