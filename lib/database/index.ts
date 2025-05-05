aea10f287ee3f1c8fde7f593ec05ffc5efac24d2
OmarChouchane <omar.ch52831@gmail.com>
2025-05-05 01:32:27 +0100

    fix

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,19 +2,33 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const cached = (global as any).mongoose || {conn: null, promise:null};
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// Extend the global object to include `mongoose`
+declare global {
+  var mongoose: MongooseCache;
+}
+
+// Ensure global.mongoose exists
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.connect;
+  if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    })
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: 'evently',
+      bufferCommands: false,
+    });
 
-    cached.conn = await cached.promise;
+  cached.conn = await cached.promise;
+  global.mongoose = cached;
 
-    return cached.conn;
-}
+  return cached.conn;
+};
