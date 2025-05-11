a43a10bbb05dd3a4a9c992696f896343b484d73b
OmarChouchane <omar.ch52831@gmail.com>
2025-05-11 02:22:25 +0100

    orders

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -4,19 +4,24 @@ import React from 'react'
 import Collection from '@/components/shared/Collection'
 import { auth } from '@clerk/nextjs/server'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { SearchParamProps } from '@/types'
 
-const page = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
 
     const { sessionClaims } = await auth();
     const userId = sessionClaims?.userId as string;
 
-    const organizedEvents = await getEventsByUser({userId, page: 1})
+    const ordersPage = Number(searchParams?.ordersPage) || 1;
+    const eventsPage = Number(searchParams?.ordersPage) || 1;
+
+
+    const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
     return (
         <>
             {/* MY EVENTS */}
             <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
                 <div className='wrapper flex items-center justify-center sm:justify-between'>
-                    <h3 className='h3-bold text-center sm:text-left'>My Events</h3>
+                    <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
                     <Button asChild size="lg" className='button hidden sm:flex'>
                         <Link href="/#events">
                             Explore More Events</Link>
@@ -47,7 +52,7 @@ const page = async () => {
                 />
             </section>
 
-            
+
 
             {/*<section className='wrapper my-8'>
                 <Collection
@@ -65,4 +70,4 @@ const page = async () => {
     )
 }
 
-export default page
+export default ProfilePage
