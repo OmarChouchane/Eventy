e0519cf0ab2c27f10eaa781d2abb48f8b189340e
OmarChouchane <omar.ch52831@gmail.com>
2025-06-04 19:06:50 +0100

    only admin access dashboard

--- app/dashboard/resources/page.tsx
+++ app/dashboard/resources/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+
 import React from 'react'
 import ResourceCatalog from '@/components/shared/ResourceCatalog'
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { useUser } from '@clerk/nextjs';
 
 const page = () => {
+
+  const { user, isLoaded, isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoaded) return; // wait until user is loaded
+
+    // Check if user is signed in and is admin
+    if (!isSignedIn || user?.publicMetadata.role !== 'admin') {
+      alert('Access denied. Admins only.');
+      router.push('/');
+    }
+  }, [isLoaded, isSignedIn, user, router]);
+
+  if (!isLoaded || !isSignedIn || user?.publicMetadata.role !== 'admin') {
+    return <div>Loading or Access Denied...</div>;
+  }
+
   return (
     <div>
       <ResourceCatalog />
