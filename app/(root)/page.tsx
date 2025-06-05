2de0ae90d50d8362edaf309c593e14dbe85af902
OmarChouchane <omar.ch52831@gmail.com>
2025-06-05 15:23:29 +0100

    trusted by

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -53,7 +53,7 @@ Because every event deserves to shine.
         </section>
 
         <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12"> 
-          <h2 className="h2-bold">Trusted by <br /> Thousands of Events</h2>
+          <h2 className="h2-bold">Trusted by <br /> INSAT Clubs and Events</h2>
 
           <div className="flex w-full flex-col gap-5 md:flex-row">
             <Search />
