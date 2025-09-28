f634deadef4456883dccc9dbd48d8cfba071dde3
OmarChouchane <omar.ch52831@gmail.com>
2025-09-28 20:20:34 +0100

    navbar hover glow effect

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,9 +7,9 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="w-full bg-transparent">
+    <header className="w-full bg-transparent ">
       <div className="wrapper py-3">
-        <div className="mx-auto flex items-center justify-between rounded-full border border-white/40 bg-white/5 px-4 py-5 backdrop-blur-md shadow-sm max-w-4xl text-white">
+        <div className="mx-auto flex items-center justify-between rounded-full border border-white/40 bg-white/5 px-4 py-5 backdrop-blur-md shadow-sm max-w-4xl text-white  transition-shadow duration-200 hover:shadow-[0_0_16px_1px_rgba(255,255,255,0.5)] ">
           <Link href="/" className="w-36 pl-2">
             <Image
               src="/logos/eventia-wh-lg.png"
@@ -31,7 +31,11 @@ const Header = () => {
               <MobileNav />
             </SignedIn>
             <SignedOut>
-              <Button asChild className="rounded-full" size="lg">
+              <Button
+                asChild
+                className="rounded-full transition-shadow duration-300 hover:shadow-[0_0_16px_4px_rgba(255,255,255,0.5)]"
+                size="lg"
+              >
                 <Link href="/sign-in" className="text-white">
                   Login
                 </Link>
